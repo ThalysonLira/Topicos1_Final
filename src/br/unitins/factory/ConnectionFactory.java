db8be4d92f272823a1dedf86bcca054e9843e96f
PazJulia <paz.julia7@gmail.com>
2019-06-11 20:36:34 -0300

    Update ConnectionFactory.java

--- src/br/unitins/factory/ConnectionFactory.java
+++ src/br/unitins/factory/ConnectionFactory.java
@@ -5,20 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	
-	private ConnectionFactory( ) {
+
+	private ConnectionFactory() {
 		// construtor como privado para nao criar uma instancia
 	}
-	
+
 	public static Connection getInstance() {
 		Connection conn = null;
-		
+
 		try {
 			// registrando o driver do postgres
 			Class.forName("org.postgresql.Driver");
 			// estabelecendo um conexao com o banco
-			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/topicos1_final",
-															"topicos1", "123456");
+			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/topicos1_final", "topicos1", "123456");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Falha ao registrar o Driver de banco");
 			e.printStackTrace();
@@ -26,7 +25,7 @@ public class ConnectionFactory {
 			System.out.println("Falha ao conectar ao banco de dados");
 			e.printStackTrace();
 		}
-		
+
 		return conn;
 	}
 }
